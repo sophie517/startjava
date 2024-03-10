37ed39e5ba3ced4e09ef33014a85e7f7b13db921
sophie517 <sophieartemeva@gmail.com>
2024-03-10 18:02:24 +0300

    implement hw_1 from lesson_2

---
+++ Lesson_1/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String gender = "male";
+    String name = "John";
+    float height = 183.5f;
+    float weight = 70.2f;
+    int age = 32;
+
+    void walk() {
+        System.out.println("Человек идет");
+    }
+
+    void sit() {
+        System.out.println("Человек сидит");
+    }
+
+    void run() {
+        System.out.println("Человек бежит");
+    }
+
+    void talk() {
+        System.out.println("Человек говорит");
+    }
+
+    void learnJava() {
+        System.out.println("Человек учит Java");
+    }
+}
