74bff9bf604a204de82074bfe254e42ef502f3f5
sophie517 <sophieartemeva@gmail.com>
2024-05-04 18:46:24 +0300

    implement hw

---
+++ src/com/startjava/lesson_5/populate.sql
@@ -0,0 +1,12 @@
+INSERT INTO jaegers(model_name, mark, height, weight, status, origin, launch, kaiju_kill) 
+     VALUES
+        ('Brawler Yukon',    'Mark-1', '81.67', '43.354', 'Destroyed', 'USA',       '2015', '2'),
+        ('Cherno Alpha',     'Mark-1', '85.34', '54.487', 'Destroyed', 'Russia',    '2015', '6'),
+        ('Valor Omega',      'Mark-6', '77.35', '40.512', 'Destroyed', 'USA',       '2018', '4'),
+        ('Vanguard Horizon', 'Mark-6', '81.77', '45.270', 'Active',    'USA',       '2034', '1'),
+        ('Coyote Tango',     'Mark-1', '85.34', '52.228', 'Destroyed', 'Japan',     '2015', '2'),
+        ('Titan Redeemer',   'Mark-6', '74.07', '50.963', 'Destroyed', 'USA',       '2035', '2'),
+        ('Horizon Bravo',    'Mark-4', '72.54', '7.890',  'Destroyed', 'China',     '2035', '1'),
+        ('Guardian Bravo',   'Mark-6', '73.21', '44.615', 'Active',    'Russia',    '2033', '1'),
+        ('Saber Athena',     'Mark-6', '76.91', '36.777', 'Destroyed', 'USA',       '2033', '1'),
+        ('Storm Garuda',     'Mark-7', '82.30', '47.890', 'Active',    'Indonesia', '2035', '1');
